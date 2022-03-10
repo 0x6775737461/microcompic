347950a1a7ae2f5a20deaf5863b4395592c8ba71
0x6775737461 <fabiogustavo@tuta.io>
2022-03-10 08:55:33 -0400

    corrigindo o caminho do arquivo de cabeçalho e mostrando outra forma de 'flipar' o bit

--- src/0_blink_led/blink_led.c
+++ src/0_blink_led/blink_led.c
@@ -1,16 +1,22 @@
-#include "../h_files/ping_config.h"
+#include "../../h_files/pin_config.h"
 
 #include <xc.h>
 
 void main(void) {
   TRISBbits.TRISB0 = 0;
+  PORTBbits.RB0 = 0;
 
   while(1){
+    PORTBbits.RB0 = ~(PORTBbits.RB0);
+
+    __delay_ms(1000);
+    /*
     PORTBbits.RB0 = 1;
     __delay_ms(1000);
 
     PORTBbits.RB0 = 0;
     __delay_ms(1000);
+    */
   }
   
   return;
