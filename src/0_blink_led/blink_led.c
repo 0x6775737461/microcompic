0207c9f2738ddf11bef9e16b377e39ad74492ec5
0x6775737461 <fabiogustavo@tuta.io>
2022-03-15 16:52:42 -0400

    alguns comentários para o esclarecimento do código feito

--- src/0_blink_led/blink_led.c
+++ src/0_blink_led/blink_led.c
@@ -3,20 +3,20 @@
 #include <xc.h>
 
 void main(void) {
+  // Configurando o DDR (Data Direction Register)
+  // para SAÍDA de dados.
   TRISBbits.TRISB0 = 0;
+
+  // Deixando o RB0 em 0V (Volts)
   PORTBbits.RB0 = 0;
 
   while(1){
+    // O operador '~' (tilde) é um operador bit-a-bit
+    // que inverte os bits de uma variável.
+    // Pesquise: Operador de Complemento a 1
     PORTBbits.RB0 = ~(PORTBbits.RB0);
 
     __delay_ms(1000);
-    /*
-    PORTBbits.RB0 = 1;
-    __delay_ms(1000);
-
-    PORTBbits.RB0 = 0;
-    __delay_ms(1000);
-    */
   }
   
   return;
