ad5331aabf264fdc629294283cb0d056a926d18c
0x6775737461 <fabiogustavo@tuta.io>
2022-03-15 16:58:31 -0400

    mais alguns comentários

--- src/1_button_and_led/button_and_led.c
+++ src/1_button_and_led/button_and_led.c
@@ -1,18 +1,25 @@
 #include <xc.h>
 
 int main(void) {
+  // RB5 (Pino 11) = Entrada
+  // RB1 (Pino 7) = Saída
   TRISBbits.TRISB5 = 1;
   TRISBbits.TRISB1= 0;
 
   PORTBbits.RB1 = 0;
 
   while(1) {
+    // Considerando que ligação de RB5
+    // foi feita com um resistor pull-down
     if(PORTBbits.RB5 == 1){
+      // Se o botão for pressionado,
+      // liga o led.
       PORTBbits.RB1 = 1;
       continue;
     }
 
+    // Se o botão estiver 'solto', o led continua
+    // desligado.
     PORTBbits.RB1 = 0;
   }
-
 }
